6cb38b647623e4f539a3e39da4bab65e8ef74d14
Alex Shevchenko <shevaroller@gmail.com>
2019-04-08 21:19:42 -0700

    #1 Filter out unused fields from the list of launches

--- server/routes/launches.js
+++ server/routes/launches.js
@@ -2,7 +2,18 @@ const API_ENDPOINT = "https://launchlibrary.net/1.4/launch";
 const fetch = require("node-fetch");
 
 module.exports = async (req, res) => {
+    // Get a list of launches from external API
     let fetchLaunches = await fetch(API_ENDPOINT);
-    let launches = await fetchLaunches.json();
-    res.send(launches);
+    let { launches } = await fetchLaunches.json();
+
+    // Filter out unused fields before sending to a client
+    let filteredLaunches = launches.map(launch => {
+        return {
+            id: launch.id,
+            name: launch.name,
+            net: launch.net
+        }
+    });
+
+    res.send(filteredLaunches);
 };
