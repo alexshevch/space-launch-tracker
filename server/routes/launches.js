b6644ca9bc3a42cdfd5bd6eb24fd0fb830e40870
Alex Shevchenko <shevaroller@gmail.com>
2019-04-08 21:11:37 -0700

    #1 Fetch a list of launches from external api

--- server/routes/launches.js
+++ server/routes/launches.js
@@ -1,3 +1,8 @@
-module.exports = (req, res) => {
-    res.send("List of launches.");
+const API_ENDPOINT = "https://launchlibrary.net/1.4/launch";
+const fetch = require("node-fetch");
+
+module.exports = async (req, res) => {
+    let fetchLaunches = await fetch(API_ENDPOINT);
+    let launches = await fetchLaunches.json();
+    res.send(launches);
 };
